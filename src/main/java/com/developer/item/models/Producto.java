0b0dca5f876652d8332ae6cf790952a5a6bd9cd8
Santiago Betancur <santiago-betancur@hotmail.com>
2021-01-27 21:05:18 -0500

    Se crea micro servicio item Java 11, cliente, con SpringBoot web, Devtools, Maven, Rest Template Client.

---
+++ src/main/java/com/developer/item/models/Producto.java
@@ -0,0 +1,45 @@
+package com.developer.item.models;
+
+import java.time.LocalDate;
+
+
+public class Producto {
+	
+
+	private Long id;
+	private String nombre;
+	private Double precio;
+	//Nuevos valores de Java8 en adelante
+	private LocalDate fechaCreacion;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public LocalDate getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(LocalDate fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/com/developer/item/SprintbootServicioItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootServicioItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
